64d37fed9ba114691ce950bb5504d5d9c38aed60
lmontenegro <montenegro.luisf@gmail.com>
2025-02-04 16:14:50 -0300

    some changes

--- data/config.tsx
+++ data/config.tsx
@@ -1,7 +1,4 @@
-import { Button } from '@chakra-ui/react'
-import { Link } from '@saas-ui/react'
 import { NextSeoProps } from 'next-seo'
-import { FaGithub, FaTwitter } from 'react-icons/fa'
 import { FiCheck } from 'react-icons/fi'
 import { Logo } from './logo'
 
@@ -11,8 +8,6 @@ const siteConfig = {
     title: '77 VSL',
     description: '2025 - 77 Venture Studio Labs.',
   } as NextSeoProps,
-  termsUrl: '#',
-  privacyUrl: '#',
   header: {
     links: [
       {
@@ -31,34 +26,6 @@ const siteConfig = {
     ],
   },
   footer: {},
-  signup: {
-    title: 'Start building with Saas UI',
-    features: [
-      {
-        icon: FiCheck,
-        title: 'Accessible',
-        description: 'All components strictly follow WAI-ARIA standards.',
-      },
-      {
-        icon: FiCheck,
-        title: 'Themable',
-        description:
-          'Fully customize all components to your brand with theme support and style props.',
-      },
-      {
-        icon: FiCheck,
-        title: 'Composable',
-        description:
-          'Compose components to fit your needs and mix them together to create new ones.',
-      },
-      {
-        icon: FiCheck,
-        title: 'Productive',
-        description:
-          'Designed to reduce boilerplate and fully typed, build your product at speed.',
-      },
-    ],
-  },
 }
 
 export default siteConfig
